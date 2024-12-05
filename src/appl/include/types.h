be3448cb3f0dd8233ea0a11f846b7d355c30e522
Oliver <oliverchen123@aliyun.com>
2024-12-05 13:47:55 +0800

    add firmware for wifi

---
+++ src/appl/include/types.h
@@ -0,0 +1,15 @@
+/**
+ * \file
+ * \brief Public Types
+ * \details This header file describes public types
+ */
+
+
+#ifndef __APPL_TYPES_H__
+#define __APPL_TYPES_H__
+
+
+
+
+
+#endif /* __APPL_TYPES_H__ */
